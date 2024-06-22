be4dd0b9fa8e4c23a73d1b007ada5d9a067292f1
Aaron Chen <chenaaron3@gmail.com>
2024-06-22 16:42:42 -0700

    comment out

--- next.config.js
+++ next.config.js
@@ -14,11 +14,11 @@ const config = {
    *
    * @see https://github.com/vercel/next.js/issues/41980
    */
-  i18n: {
-    locales: ["en"],
-    defaultLocale: "en",
-  },
-  transpilePackages: ["geist"],
+  // i18n: {
+  //   locales: ["en"],
+  //   defaultLocale: "en",
+  // },
+  // transpilePackages: ["geist"],
 };
 
 export default config;
